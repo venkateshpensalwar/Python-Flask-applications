c0f012eacbe008a8070dcc8cb691193b80e7c026
venatesh <venkateshpensalwar@gmail.com>
2021-06-09 18:07:01 +0530

    FLask Ajax Example

---
+++ Flask_Ajax/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template, request
+
+app = Flask("AJAX")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sub', methods=['POST'])
+def sub():
+    name = request.form['name']
+    return  name
